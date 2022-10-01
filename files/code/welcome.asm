8cc481b58f08d3225b571e482d27fae1f2f05e60
dlandahl <36662196+dlandahl@users.noreply.github.com>
2022-10-01 19:44:54 +0100

    System of licenses and regulations

--- files/code/welcome.asm
+++ files/code/welcome.asm
@@ -2,13 +2,12 @@
 syscall_write :: 0x3;
 iterations :: 5;
 
-message: "Hello world: 1"
-newline: 10;
+message := "Hello world: 1";
+newline := 10;
 
 mov rax, syscall_write;
 mov rbx, message.data;
-mov rcx, message.count;
-add rcx, 2;
+mov rcx, message.count + 1;
 
 mov rdi, iterations;
 mov rdx, message.data + message.count - 1;
